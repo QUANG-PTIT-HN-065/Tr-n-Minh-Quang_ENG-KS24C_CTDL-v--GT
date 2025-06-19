b375d3be8f9aa8f7aec866be98e4cce6b60ed800
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2025-06-19 23:02:11 +0700

    PTIT_CNTT1_Session02_Bai05.cpp

--- PTIT_CNTT1_Session02_Bai05.cpp
+++ PTIT_CNTT1_Session02_Bai05.cpp
@@ -28,7 +28,7 @@ int main() {
     for (int i = pos; i < n - 1; i++) {
         arr[i] = arr[i + 1];
     }
-    
+    n--;
     for (int i = 0; i < n; i++) {
         cout << "arr[" << i << "] = " << arr[i] << endl;
     }
